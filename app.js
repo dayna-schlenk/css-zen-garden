b376ed26604837c232d461f8f2a9aaf5a1e03d5b
dayna-schlenk <dayna.schlenker@gmail.com>
2022-11-26 12:05:34 -0600

    Clean up navbar code and add comments

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const navbar = document.querySelector(".main-nav");
+// returns a DOMRect object providing info about the size of an element & its position relative to viewport
 const navPosition = navbar.getBoundingClientRect().top;
 
-window.addEventListener("scroll", evt => {
+window.addEventListener("scroll", function() {
+    // returns the number of px that document is currently scrolled vertically
     let scrollPosition = window.scrollY;
 
     if (scrollPosition > navPosition) {
         navbar.classList.add("sticky");
-        Headers.classList.add("navbarOffsetMargin");
     } else {
         navbar.classList.remove("sticky");
-        Headers.classList.remove("navbarOffsetMargin");
     }
 });
