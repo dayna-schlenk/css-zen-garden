ac18edd9ccb95b609c53fddcfecbefa021b9279d
dayna-schlenk <dayna.schlenker@gmail.com>
2022-11-26 14:52:37 -0600

    Comment out js, reset sticky nav styling

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const navbar = document.querySelector(".main-nav");
+// const navbar = document.querySelector(".main-nav");
+
 // returns a DOMRect object providing info about the size of an element & its position relative to viewport
-const navPosition = navbar.getBoundingClientRect().top;
+// const navPosition = navbar.getBoundingClientRect().top;
 
-window.addEventListener("scroll", function() {
-    // returns the number of px that document is currently scrolled vertically
-    let scrollPosition = window.scrollY;
+// window.addEventListener("scroll", function() {
+//     // returns the number of px that document is currently scrolled vertically
+//     let scrollPosition = window.scrollY;
 
-    if (scrollPosition > navPosition) {
-        navbar.classList.add("sticky");
-    } else {
-        navbar.classList.remove("sticky");
-    }
-});
+//     if (scrollPosition > navPosition) {
+//         navbar.classList.add("sticky");
+//     } else {
+//         navbar.classList.remove("sticky");
+//     }
+// });
